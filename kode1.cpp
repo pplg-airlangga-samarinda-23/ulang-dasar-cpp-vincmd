4400a4f052927a772c10913d3cd8669bbe5e3e67
Vincent <23_vincenttheodores@student.smkti.net>
2024-07-31 11:24:36 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+    void cetakdata(string nama,string nisn,string jurusan,int beratbadan);
+int main(){
+    int jadi,input;
+    cout <<"masukkan nama<<\n";
+    cin >> nama;
+    cout <<"masukkan nisn<<\n";
+    cin >> nisn;
+    cout <<"masukkan jurusan\n";
+    cin >> jurusan;
+    cout <<"masukkan tinggi badan\n";
+    cin >> beratbadan
+    jadi=cetakdata(nama,nisn,jurusan,beratbadan);
+    cout << "hasilnya yang anda buat adalah" <<jadi <<endl;
+
 }
